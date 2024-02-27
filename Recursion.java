e398b250fed4c7fb1a00e0b86ced521bdbae7463
Pankaj Singh <p7ksingh@gmail.com>
2024-02-27 11:56:33 +0530

    Resursion

---
+++ Recursion.java
@@ -0,0 +1,17 @@
+package com.arr.recursion;
+
+//StackOverflowError
+public class Recursion{
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        int smallOutput=fact(n-1);
+        int output=n*smallOutput;
+        return output;
+        
+    }
+    public static void main(String[] args){
+        System.out.println(fact(5));
+    }
+}
